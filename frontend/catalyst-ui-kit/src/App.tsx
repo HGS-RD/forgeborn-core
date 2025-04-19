bcaca93f8578f75aedef3b11f0bfd633a8b15d62
Roger Hill <roger.hill@hillstrongsecurity.com>
2025-04-19 13:02:32 -0400

    Initializing Catalyst UI

---
+++ frontend/catalyst-ui-kit/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Catalyst UI Kit is running!</h1>
+    </div>
+  )
+}
